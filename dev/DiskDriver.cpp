86cdd0e97b84f955f3a3bccf231977d72df1568d
Kagaya <kagaya85@outlook.com>
2019-04-10 23:57:46 +0800

    diskdriver

--- dev/DiskDriver.cpp
+++ dev/DiskDriver.cpp
@@ -1,1 +1,53 @@
 #include "DiskDriver.h"
+#include "FileSystem.h"
+
+DiskDriver::DiskDriver()
+{
+    /* nothin to do */
+}
+
+DiskDriver::~DiskDriver()
+{
+    close(d_diskfileFd);
+}
+
+/**
+ * 初始化
+ */
+int DislDriver::Initialize()
+{
+    if(access(DISK_FILE_NAME, F_OK) < 0)
+    {
+        cerr << "\"" << DISK_FILE_NAME << "\" dose not exit" << endl;
+        exit(-1);
+    }
+
+    d_diskfileFd = open(DISK_FILE_NAME, O_RDWR);
+    if(d_diskfileFd < 0)
+    {
+        cerr << "open disk file error" << endl;
+        exit(-1);
+    }
+}
+
+/**
+ * 从disk读入到bp所控制的缓存块
+ */
+int DiskDriver::ReadFromDisk(Buf* bp)
+{
+    unsigned char* b_addr = bp->b_addr;
+    int bln = bp->b_blkno;
+    lseek(d_diskfileFd, bln * FileSystem::BLOCK_SIZE, SEEK_SET);
+    read(d_diskfileFd, b_addr, FileSystem::BLOCK_SIZE);
+}
+
+/**
+ * 将bp控制的缓存块写入disk
+ */
+int DiskDriver::WriteToDisk(Buf* bp)
+{
+    unsigned char* b_addr = bp->b_addr;
+    int bln = bp->b_blkno;
+    lseek(d_diskfileFd, bln * FileSystem::BLOCK_SIZE, SEEK_SET);
+    write(d_diskfileFd, b_addr, FileSystem::BLOCK_SIZE);
+}
--- include/DiskDriver.h
+++ include/DiskDriver.h
@@ -1,9 +1,50 @@
 /*
  * 主要负责对模拟的磁盘文件进行基本的IO读写操作 
  */
-#include<stdlib.h>
+#ifndef DISKDRIVER
+#define DISKDRIVER
 
+#include <stdlib.h>
+#include <Buf.h>
+#include <io.h>
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include "Defines.h"
+
+/**
+ * 块设备表devtab定义 
+ */
+class Devtab
+{
+public:
+	Devtab();
+	~Devtab();
+	
+public:
+	int	d_active;
+	int	d_errcnt;
+	Buf* b_forw;	// 指向最后IO扇区
+	Buf* b_back;
+	Buf* d_actf;	// IO请求队列队首
+	Buf* d_actl;
+};
+
+/**
+ * 提供块设备随机读写操作
+ */
 class DiskDriver
 {
-
+private:
+    int d_diskfileFd;   // 磁盘文件fd
+public:
+	Devtab*	d_tab;		/* 指向块设备表的指针 */
+public:
+    DiskDriver();
+    ~DiskDriver();
+	int Initialize();
+    int ReadFromDisk(Buf* bp);
+    int WriteToDisk(Buf* bp);
 };
+
+#endif DISKDRIVER
